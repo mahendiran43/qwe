8818b4c51a9bfc575cc1989183e5d6625fb24c1c
mahendiran43 <kmahee000@gmail.com>
2016-07-10 14:57:39 +0530

    finish

---
+++ README.c
@@ -0,0 +1,22 @@
+# qwe
+#include <stdio.h>
+void char_num(char num[3], int n) 
+{
+    if(n > 0) 
+    {
+        num[3-n] = '0';
+        char_num(num, n - 1);
+        num[3-n] = '1';
+        char_num(num, n - 1);
+    }
+    else 
+        printf("%s\n", num);
+}
+int main() 
+{
+    char num[4] = {0};
+    int n;
+    scanf("%d",&n);
+    char_num(num,n);
+    return 0;
+}
